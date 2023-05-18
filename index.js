03b14b40de19402e2db0f83720c320c63ac081c6
Mohammad Saiful Islam <saifmdislam231@gmail.com>
2023-05-18 07:05:12 -0700

    search toys by toy name function added

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 
 const port = process.env.PORT || 5000;
 const app = express();
 
-// midlewire 
+// midlewire
 app.use(cors());
 app.use(express.json());
 
-
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kgqetuh.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -20,45 +18,52 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    const toyCollection = client.db('toyLand').collection('toys')
+    const toyCollection = client.db("toyLand").collection("toys");
 
+    // Creating index on subcategory fields
+    const indexKeys = { name: 1 };
+    const indexOptions = { name: "name" };
+    const result = await toyCollection.createIndex(indexKeys, indexOptions);
+    console.log(result);
 
-        // Creating index on subcategory fields
-        const indexKeys = { subcategory: 1};
-        const indexOptions = { name: "subCategory" };
-        const result = await toyCollection.createIndex(indexKeys, indexOptions);
-        console.log(result);
+    app.get("/all-toys", async (req, res) => {
+      const result = await toyCollection.find().limit(20).toArray();
+      res.send(result);
+    });
 
-
-    app.get('/all-toys', async(req,res)=>{
-      const result = await toyCollection.find().toArray();
-      res.send(result)
-    })
-
-    app.get('/toys-by-subCategory/:text', async(req, res)=>{
+    // get toys by subCategory
+    app.get("/toys-by-subCategory/:text", async (req, res) => {
       const subCategory = req.params.text;
-      console.log({subCategory})
+      console.log({ subCategory });
 
-      const query = {subcategory : subCategory };
-        const result = await toyCollection.find(query).toArray();
-        return res.send(result);
+      const query = { subcategory: subCategory };
+      const result = await toyCollection.find(query).toArray();
+      return res.send(result);
+    });
 
-    } )
+    // search toys by name
+    app.get('/toysByName/:searchText', async(req, res)=>{
+      const searchText = req.params.searchText;
+      console.log(searchText)
 
+      const query = { name: { $regex: searchText, $options: "i" } };
+      const result = await toyCollection.find(query).limit(20).toArray();
+      res.send(result);
 
-
-
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -66,14 +71,10 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("Welcome to ToyLand Server");
+});
 
-
-app.get('/', (req, res)=>{
-    res.send("Welcome to ToyLand Server")
-})
-
-
-
-app.listen(port, ()=>{
-    console.log(`ToyLand server is running on port: ${port}`)
-})
+app.listen(port, () => {
+  console.log(`ToyLand server is running on port: ${port}`);
+});
