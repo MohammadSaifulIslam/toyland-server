af0929a01be449cd5e67aa7ffbff688869bc2b1f
Mohammad Saiful Islam <saifmdislam231@gmail.com>
2023-05-18 21:00:23 -0700

    single toy details send to client side function added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 
 const port = process.env.PORT || 5000;
@@ -9,7 +10,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kgqetuh.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -33,10 +33,19 @@ async function run() {
     const result = await toyCollection.createIndex(indexKeys, indexOptions);
     console.log(result);
 
+    // get all toys
     app.get("/all-toys", async (req, res) => {
       const result = await toyCollection.find().limit(20).toArray();
       res.send(result);
     });
+
+    // get single toy details
+    app.get('/toy/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id : new ObjectId(id)}
+      const result = await toyCollection.findOne(query);
+      res.send(result);
+    })
 
     // get toys by subCategory
     app.get("/toys-by-subCategory/:text", async (req, res) => {
